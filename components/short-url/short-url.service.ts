d122e76a3299af98056514761b35625391d7d4dd
Wilson Gouanet <wilstito@gmail.com>
2021-11-18 09:51:10 -0500

    fix url resolution

--- components/short-url/short-url.service.ts
+++ components/short-url/short-url.service.ts
@@ -1,13 +1,14 @@
 import * as util from 'util';
 import * as dns from 'dns';
+import * as url from 'url';
 import { v4 } from 'uuid';
 import ShortUrlRepository from './short-url.repository';
 
 const ShortUrlService = {
   async createShortUrl(originalUrl: string) {
     try {
-      const response = await util.promisify(dns.lookup)(originalUrl);
-      console.log(response);
+      const q = new url.URL(originalUrl);
+      await util.promisify(dns.lookup)(q.hostname);
       return ShortUrlRepository.insert({ original_url: originalUrl, short_url: v4() });
     } catch (e) {
       throw new Error('invalid url');
